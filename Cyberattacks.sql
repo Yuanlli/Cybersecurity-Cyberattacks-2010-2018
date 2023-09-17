a774560ea6b7ee505bf39119876febf81f18ed08
Yuan <35889216+Yuanlli@users.noreply.github.com>
2023-09-18 00:16:35 +0800

    Update Cyberattacks.sql

--- Cyberattacks.sql
+++ Cyberattacks.sql
@@ -7,4 +7,8 @@ select *
 from dbo.[cyberattacks cleaned.xlsx]
 where Method = 'unknown'
 
-
+-- 2 Organisations experienced cyber attacks with unknown methods. MyHeritage and Bedford/St. Martins
+  
+-- Clean the list by removing the 2 organisation that are have unknown cyberattack methods
+  
+DELETE FROM dbo.[cyberattacks cleaned.xlsx] WHERE Organisation = 'MyHeritage' and 'Bedford/St. Martin''s'
